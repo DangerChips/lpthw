869ce7f2979b12e3acd58e171606a58739ad9cbb
Brian Smith <Brian.LP.Smith@Gmail.com>
2016-05-02 12:19:30 -0700

    more lessons completed (up to 35)

--- ex31.py
+++ ex31.py
@@ -5,18 +5,18 @@ door = raw_input("> ")
 if door == "1":
 	print "There's a giant bear here eating a cheese cake.  What do you do?"
 	print "1. Take the cake."
-    print "2. Scream at the bear."
+	print "2. Scream at the bear."
 
-    bear = raw_input("> ")
+	bear = raw_input("> ")
 
-    if bear == "1":
-    	print "The bear eats your face off. Good job!"
-    elif bear == "2":
-    	print "The bear eats your legs off. Good job!"
-    else:
-    	print "Well, doing %s is probably better. Bear runs away." % bear
+	if bear == "1":
+		print "The bear eats your face off. Good job!"
+	elif bear == "2":
+		print "The bear eats your legs off. Good job!"
+	else:
+		print "Well, doing %s is probably better. Bear runs away." % bear
 
-elif door = "2":
+elif door == "2":
 	print "You star into the endless abyss of Cthulu's retina."
 	print "1. Blueberries."
 	print "2. Yellow jacket clothespins."
